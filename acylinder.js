08fc96f9869e25b48c9e4fb5f4ff770db59c59a7
fioalvsera <fioalvsera@gmail.com>
2021-02-17 15:01:14 -0600

    re-factor of strings

--- acylinder.js
+++ acylinder.js
@@ -1,11 +1,8 @@
 var radioBase = parseInt(process.argv[2])
 var height = parseInt(process.argv[3])
-console.log("the radio of the base is")
-console.log(radioBase)
-console.log("the height is")
-console.log(height)
+console.log("the radio of the base is " + radioBase)
+console.log("the height is " + height)
 
 var area = (radioBase * radioBase) * 3.14
 var volume = area * height 
-console.log("the volume is")
-console.log(volume)
+console.log("the volume is " + volume)
