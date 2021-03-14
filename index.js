03509ddcdd7ca71e6b2cda8d587847098f6d99b6
fioalvsera <fioalvsera@gmail.com>
2021-03-14 09:31:27 -0600

    adding index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+//node index.js cylinder [radio] [altura]
+//node index.js square [lado]
+
+var comandType = process.argv[2]
+
+if (comandType == "a"){
+ console.log("entro")
+} else if (comandType == "b"){
+ console.log("no entro")
+}
