df84eead438ee47380bbfec2eeb6b413a1f5ea02
fioalvsera <fioalvsera@gmail.com>
2021-03-26 13:56:47 -0600

    fixing wrong switch statement

--- index.js
+++ index.js
@@ -3,9 +3,10 @@
 
 var shape = process.argv[2] // [shape] cylynder || square
 
+let area = 0
 
-siwtch(shape,);{
-    case "cylinder";
+switch(shape){
+    case "cylinder":
         let radioBase = process.argv[3] // [radius] || [side]
         let height = process.argv[4] // [height]
 
@@ -13,21 +14,21 @@ siwtch(shape,);{
         console.log("the radio of the base is " + radioBase)
         console.log("the height is " + height)
 
-        let area = (radioBase * radioBase) * 3.14
-        let volume = area * height 
+        area = (radioBase * radioBase) * 3.14
+        let volume = area * height
         console.log("the volume is " + volume)
         break;
 
-    case "square";
+    case "square":
         let side = process.argv[3] // [radius] || [side]
 
         console.log("square")
-        console.log("the side's lenth is " + side)
+        console.log("the side's length is " + side)
 
-        let area = side * side 
-        console.log("the area is " + area) 
+        area = side * side
+        console.log("the area is " + area)
         break;
 
-    default
+    default:
         console.log ("Sorry that geometric shape is not supported.")
-}   
+}
