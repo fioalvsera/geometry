96ef6e289ba771b6ebdfee0ee2011193bb862903
fioalvsera <79225699+fioalvsera@users.noreply.github.com>
2021-03-26 14:06:25 -0600

    changing if statements to switch statements (#5)
    * changing if statements to switch statements
    
    * fixing wrong switch statement
    
    * adding blank at EOF

--- index.js
+++ index.js
@@ -2,30 +2,33 @@
 //node index.js square [side]
 
 var shape = process.argv[2] // [shape] cylynder || square
- 
-if ( shape == "cylinder") {
-  
-  let radioBase = process.argv[3] // [radius] || [side]
-  let height = process.argv[4] // [height]
 
-  console.log("cylinder")
-  console.log("the radio of the base is " + radioBase)
-  console.log("the height is " + height)
+let area = 0
 
-  let area = (radioBase * radioBase) * 3.14
-  let volume = area * height 
-  console.log("the volume is " + volume)
+switch(shape){
+    case "cylinder":
+        let radioBase = process.argv[3] // [radius] || [side]
+        let height = process.argv[4] // [height]
 
-} else if (shape == "square"){
-  
-  let side = process.argv[3] // [radius] || [side]
+        console.log("cylinder")
+        console.log("the radio of the base is " + radioBase)
+        console.log("the height is " + height)
 
-  console.log("square")
-  console.log("the side's lenth is " + side)
+        area = (radioBase * radioBase) * 3.14
+        let volume = area * height
+        console.log("the volume is " + volume)
+        break;
 
-  let area = side * side 
-  console.log("the area is " + area) 
+    case "square":
+        let side = process.argv[3] // [radius] || [side]
 
-} else {
-  console.log ("Sorry that geometric shape is not supported.")
+        console.log("square")
+        console.log("the side's length is " + side)
+
+        area = side * side
+        console.log("the area is " + area)
+        break;
+
+    default:
+        console.log ("Sorry that geometric shape is not supported.")
 }
