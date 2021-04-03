70674c729b281b2dbc7f358173a40edf67caee48
fioalvsera <fioalvsera@gmail.com>
2021-04-02 21:40:46 -0600

    adding scripts for calculating areas of a circle and rectangle

---
+++ arect.js
@@ -0,0 +1,10 @@
+var base = parseInt(process.argv[2])
+var height = parseInt(process.argv[3])
+console.log("the base's lenth is")
+console.log(base)
+console.log("the height's lenth is")
+console.log(height)
+
+var area = base * height
+console.log("the area is")
+console.log(area)
