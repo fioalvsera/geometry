9dcd16ffe312cbc3b863ad05d867d287f86835f5
fioalvsera <fioalvsera@gmail.com>
2021-04-04 07:21:40 -0600

    correcting typo

--- arect.js
+++ arect.js
@@ -1,8 +1,8 @@
 var base = parseInt(process.argv[2])
 var height = parseInt(process.argv[3])
-console.log("the base's lenth is")
+console.log("the base's length is")
 console.log(base)
-console.log("the height's lenth is")
+console.log("the height's length is")
 console.log(height)
 
 var area = base * height
--- asquare.js
+++ asquare.js
@@ -1,7 +1,7 @@
 var side = parseInt(process.argv[2])
-console.log("the side's lenth is")
+console.log("the side's length is")
 console.log(side)
 
-var area = side * side 
+var area = side * side
 console.log("the area is")
 console.log(area)
