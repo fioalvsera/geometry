34020cd7b60befdebe03b79352783871a127ec87
fioalvsera <fioalvsera@gmail.com>
2021-08-15 08:49:32 -0600

    Integrates areaCalculator with persistenceCsv

--- areaCalculator.js
+++ areaCalculator.js
@@ -1,5 +1,6 @@
 const geometry = require("./geometry.js");
-//const persistenceCsv = require("./persistenceCsv");
+const persistenceCsv = require("./persistenceCsv");
+const { v4: uuidv4 } = require('uuid')
 //const index = require("./index.js");
 
 const readline = require('readline').createInterface({
@@ -15,51 +16,65 @@ async function readLineAsync(message) {
     });
 }
 
-let allFiguresData = []
+const writeFigure = (side, area, figureType) => {
+    let figureData = [{
+        id: uuidv4(),
+        side: side,
+        area: area.toString(),
+        figure_type: figureType
+    }]
+    console.log(figureData)
+    console.log(`Writing ${figureData.length} figures in file history`)
+    persistenceCsv.write(figureData)
+    console.log("Figures have been written succesfully")
+}
 
 const runAreaCalculator = async function()  {
     const figure = await readLineAsync("What's your figure's name?")
     let area = 0
+    let side = 0
+    let figureType = ""
+    let typeOfResult = "area"
     switch (figure) {
         case "square":
             const sideLength = await readLineAsync("What's your side's length?")
+            side = `${sideLength}`
+            console.log("Calculating area")
             area = geometry.getSquareArea(sideLength)
-            console.log(`the area of the figure is ${area}`)
-            let figureData = {
-                id: "8",
-                side: sideLength,
-                area: area.toString(),
-                figure_type: "square"
-            }
-            allFiguresData.push(figureData)
+            figureType = "square"
             break;
 
         case "rectangle":
             const heightRectangle = await readLineAsync("What's your height's length?")
             const baseRectangle = await readLineAsync("What's your bases's length?")
+            side = `${baseRectangle} & ${heightRectangle}`
+            console.log("Calculating area")
             area = geometry.getRectArea(baseRectangle,heightRectangle)
-            console.log(area)
+            figureType = "rectangle"
             break;
 
         case "cylinder":
             const baseCylinder = await readLineAsync("What's your radio's length?")
             const heightCylinder = await readLineAsync("What's your height?")
+            side = `${baseCylinder} & ${heightCylinder}`
+            console.log("Calculating volume")
             area = geometry.getCylinderVolume(baseCylinder,heightCylinder)
-            console.log(area)
+            typeOfResult = "volume"
+            figureType = "cylinder"
             break;
 
         case "circle":
             const radio = await readLineAsync("What's your radio's length?")
+            side = `${radio}`
+            console.log("Calculating area")
             area = geometry.getCircleArea(radio)
-            console.log(area)
+            figureType = "circle"
             break;
     }
+    console.log(`the ${typeOfResult} of the figure is ${area}`)
+    writeFigure(side, area, figureType)
 }
-
-runAreaCalculator()
-//console.log(allFiguresData)
 
 module.exports = {
     runAreaCalculator: runAreaCalculator
 }
-
--- index.js
+++ index.js
@@ -1,29 +1,12 @@
 const persistence = require("./persistenceCsv.js")
+const areaCalculator = require("./areaCalculator.js")
+const { runAreaCalculator } = require("./areaCalculator.js")
 
-let newFiguresData = [{
-    id: "6",
-    side: "2",
-    area: "4",
-    figure_type: "square"
-},{
-    id: "7",
-    side: "6",
-    area: "36",
-    figure_type: "square"
-}]
-
-// read all figures
-console.log(`reading figures from file: ${persistence.FILE_ADDRESS}`)
-let allFigures = persistence.getAll()
-console.log(`${allFigures.length} figures have been read`)
-
-// write new array of figures in the file in Csv format
-console.log(`writing figures in ${persistence.FILE_ADDRESS}`)
-persistence.write(newFiguresData)
-
-console.log(`${newFiguresData.length} new figures have been written`)
-
-// exports
-module.exports = {
-    newFiguresData: newFiguresData
-}
+areaCalculator.runAreaCalculator().then(() => {
+    console.log("The program has finished.")
+    process.exit(0)
+}).catch((e) => {
+    console.log("an error occured:")
+    console.log(e)
+    process.exit(2)
+})
